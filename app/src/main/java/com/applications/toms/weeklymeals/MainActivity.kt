c89ed360f79759701bf45c4db65748146d1b9521
Tomas Vazquez <tomas.manuel.vazquez@gmail.com>
2022-07-18 18:37:40 +0200

    Add Experimental API

--- app/src/main/java/com/applications/toms/weeklymeals/MainActivity.kt
+++ app/src/main/java/com/applications/toms/weeklymeals/MainActivity.kt
@@ -3,10 +3,12 @@ package com.applications.toms.weeklymeals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import com.google.accompanist.pager.ExperimentalPagerApi
 
+@ExperimentalFoundationApi
 @ExperimentalComposeUiApi
 @ExperimentalMaterialApi
 @ExperimentalPagerApi
